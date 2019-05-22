aef5f15e22c4ea06cb42133598e375e2f8028e87
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-22 11:17:05 -0700

    debugging

--- assignments/project/db/vocab_words.php
+++ assignments/project/db/vocab_words.php
@@ -1,11 +1,11 @@
 <?
     function getVocab_words($vocab_list_id){
         global $db;
-        
         $query = "SELECT * FROM vocab_words
-            WHERE vocab_list_id = $vocab_list_id
+            WHERE vocab_list_id =  :vocab_list_id
             ORDER BY name";
         $statement = $db->prepare($query);
+        $statement->bindValue(':vocab_list_id',$vocab_list_id);
         $statement->execute();
         $vocab_words = $statement->fetchAll();
         $statement->closeCursor();
