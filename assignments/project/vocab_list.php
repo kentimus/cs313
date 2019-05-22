33cee679b451dcc21a0453449c29b7aa565104ae
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-22 11:07:45 -0700

    more 500 errors

--- assignments/project/vocab_list.php
+++ assignments/project/vocab_list.php
@@ -7,8 +7,8 @@ $vocab_list_id = $_GET['id'];
 include("db/vocab_words.php");
 include("db/vocab_lists.php");
 
-$listname = getVocabListName($vocab_list_id);
-$vocab_words = getVocab_words($vocab_list_id);
+//$listname = getVocabListName($vocab_list_id);
+//$vocab_words = getVocab_words($vocab_list_id);
 
 
 ?>
