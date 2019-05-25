0c64115a4dec5a3254b71a94a2ab202eb2418ad8
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-25 16:25:14 -0700

    debug

--- assignments/project/quiz.php
+++ assignments/project/quiz.php
@@ -1,6 +1,5 @@
 <?
 session_start();
-include();
 include("db_connect.php");
 $db = get_db();
 
@@ -21,7 +20,18 @@ include("header.php");
         <div class="row">
             <div class="col-md-12">
                 
+                <h1><?=$listname;?> Quiz</h1>
                 
+                <p>(doesn't work yet)</p>
+                
+                <p>Translate:</p>
+                <p class="question"><?=$vocab_words[0]['word'];?></p>
+                
+                <p>
+                    <a href="" class="btn btn-secondary"><?=$vocab_words[0]['english_word'];?></a>
+                    <a href="" class="btn btn-secondary"><?=$vocab_words[1]['english_word'];?></a>
+                    <a href="" class="btn btn-secondary"><?=$vocab_words[2]['english_word'];?></a>
+                </p>
             </div>
         </div>
     </div>
