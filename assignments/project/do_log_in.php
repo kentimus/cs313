a314e8bed5ec4658f19e00ecd95281eb4a779d80
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-25 11:18:16 -0700

    testing login

--- assignments/project/do_log_in.php
+++ assignments/project/do_log_in.php
@@ -8,7 +8,7 @@ include('db/users.php');
 $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
 $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
-
+verifyUser($username, $password);
 
 $vocab_list_id = $_GET['id'];
 
