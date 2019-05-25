3e532b2707e365c633b4fcdbb872963e5e555f47
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-25 12:10:55 -0700

    testing vocab lists

--- assignments/project/vocab_list.php
+++ assignments/project/vocab_list.php
@@ -14,89 +14,23 @@ $vocab_words = getVocab_words($vocab_list_id);
 
 ?>
 <? include("header.php"); ?>
-
-<div class="container">
-      
-  <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div class="flippable appcon ac-bicycle">
-        <div class="front">
-          <span>B</span>
-          <i class="fa fa-bicycle"></i>
-        </div>
-        <div class="back">
-          Bicycle
-        </div>
-      </div>
-  </div>
-  
-<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div class="flippable appcon ac-car">
-        <div class="front">
-          <span>C</span>
-          <i class="fa fa-car"></i>
-        </div>
-        <div class="back">
-          Car
-        </div>
-      </div>
-  </div>
-      
-    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div class="flippable appcon ac-truck">
-        <div class="front">
-          <span>T</span>
-          <i class="fa fa-truck"></i>
-        </div>
-        <div class="back">
-          Truck
-        </div>
-      </div>
-  </div>
-  
-  <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div class="flippable appcon ac-umbrella">
-        <div class="front">
-          <span>U</span>
-          <i class="fa fa-umbrella"></i>
-        </div>
-        <div class="back">
-          umbrella
-        </div>
-      </div>
-  </div>
-    
-<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div class="flippable appcon ac-plane">
-        <div class="front">
-          <span>P</span>
-          <i class="fa fa-plane"></i>
-        </div>
-        <div class="back">
-          Plane
-        </div>
-      </div>
-  </div>
-</div>
-
-
 <div class="container">
     <div class="row">
         <div class="col-md-12">
             <h1><?=$listname;?></h1>
             
-            <table class="table table-striped">
-                <thead>
-                    
-                </thead>
-                <tbody>
-                <? foreach($vocab_words as $vw){ ?>
-                    <tr>
-                        <td><?=$vw['word'];?></td>
-                        <td><?=$vw['english_word'];?></td>
-                    </tr>
-                <? } ?>  
-                </tbody>
-            </table>
+            <? foreach($vocab_words as $vw){ ?> 
+                <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                    <div class="flippable appcon ac-bicycle">
+                        <div class="front">
+                            <?=$vw['word'];?>
+                        </div>
+                        <div class="back">
+                            <?=$vw['english_word'];?>
+                        </div>
+                    </div>
+                </div>
+            <? } ?>
             
         </div>
     </div>
