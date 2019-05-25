3650d1505ae3cbf672f120b378deb6849850f21b
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-25 11:24:45 -0700

    more debugging

--- assignments/project/db/users.php
+++ assignments/project/db/users.php
@@ -10,10 +10,10 @@ function verifyUser($username, $password){
     $statement->execute();
     $user = $statement->fetch();
     $statement->closeCursor();
-    echo "<pre>";
-    echo print_r($user);
-    echoh "</pre>";
-    echo count($user);
-    return true;
+//    echo "<pre>";
+//    echo print_r($user);
+//    echoh "</pre>";
+//    echo count($user);
+    return $user;
 }
 ?>
