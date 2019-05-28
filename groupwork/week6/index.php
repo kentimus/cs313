cfbe3e7c25be426e17849f1d1753879aa7d9861a
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-28 13:08:08 -0700

    groupwork6

--- groupwork/week6/index.php
+++ groupwork/week6/index.php
@@ -12,32 +12,39 @@ $topics = getTopics();
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <form action="new_scripture.php" method="post">
-            <p>
-                <label for="book">Book</label>
-                <input type="text" id="book" name="book">
-            </p>
-            <p>
-                <label for="chapter">Chapter</label>
-                <input type="number" id="chapter" name="chapter">
-            </p>
-            <p>
-                <label for="verse">Verse</label>
-                <input type="number" id="verse" name="verse">
-            </p>
-            <p>
-                <label for="content">Content</label>
-                <textarea id="content" name="content"></textarea>
-            </p>
-            <p>
-            <? foreach($topics as $t){ ?>
-                <label><input type="checkbox" name="topics[]" value="<?=$t['name'];?>"> <?=$t['name'];?></label><br>
-    
-            <? } ?>
-            </p>
-            <p>
-                <input type="submit" value="add scripture">
-            </p>
-        </form>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="new_scripture.php" method="post">
+                        <p>
+                            <label for="book">Book</label>
+                            <input class="form-control" type="text" id="book" name="book">
+                        </p>
+                        <p>
+                            <label for="chapter">Chapter</label>
+                            <input class="form-control" type="number" id="chapter" name="chapter">
+                        </p>
+                        <p>
+                            <label for="verse">Verse</label>
+                            <input class="form-control" type="number" id="verse" name="verse">
+                        </p>
+                        <p>
+                            <label for="content">Content</label>
+                            <textarea class="form-control" id="content" name="content"></textarea>
+                        </p>
+                        <p>
+                        <? foreach($topics as $t){ ?>
+                            <label><input type="checkbox" name="topics[]" value="<?=$t['name'];?>"> <?=$t['name'];?></label><br>
+
+                        <? } ?>
+                        </p>
+                        <p>
+                            <input class="form-control" type="submit" value="add scripture">
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
     </body>
 </html>
