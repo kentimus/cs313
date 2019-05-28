de330f218478ed714c74b4b34a4a0efbd5a94a3e
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-28 15:06:13 -0700

    groupwork6

--- groupwork/week6/stretch3.js
+++ groupwork/week6/stretch3.js
@@ -2,26 +2,28 @@ $(document).ready(function(){
     $("#new-scripture-form").on("submit", function(event){
         event.preventDefault();
         
-        var book = $("#book").val();
-        var chapter = $("#chapter").val();
-        var verse   = $("#verse").val();
-        var content = $("#content").val();
-        //var topics  = $('.checkbox-topics:checked').serialize();
-        var topics = "hello";
-        var newtopic = $("#newtopic").val();
-        var newtopictext = $("#newtopictext");
+//        var book = $("#book").val();
+//        var chapter = $("#chapter").val();
+//        var verse   = $("#verse").val();
+//        var content = $("#content").val();
+//        //var topics  = $('.checkbox-topics:checked').serialize();
+//        var topics = "hello";
+//        var newtopic = $("#newtopic").val();
+//        var newtopictext = $("#newtopictext");
+//        
+//        $.post('ajax_new_scripture.php', {
+//                'book' : book,
+//                'chapter' : chapter,
+//                'verse' : verse,
+//                'content' : content,
+//                'topics'  : topics,
+//                'newtopic' : newtopic,
+//                'newtopictext' : newtopictext
+//            } , function(data){
+//                $("#scripture-list").html(data);
+//            }, 'html'
+//        );
         
-        $.post('ajax_new_scripture.php', {
-                'book' : book,
-                'chapter' : chapter,
-                'verse' : verse,
-                'content' : content,
-                'topics'  : topics,
-                'newtopic' : newtopic,
-                'newtopictext' : newtopictext
-            } , function(data){
-                $("#scripture-list").html(data);
-            }, 'html'
-        );
+        alert("hallo!");
     });
 });
