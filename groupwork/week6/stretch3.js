d5152d1c50bdbbb61b703c2f0fb39385f42aff90
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-28 15:08:31 -0700

    groupwork6

--- groupwork/week6/stretch3.js
+++ groupwork/week6/stretch3.js
@@ -9,21 +9,21 @@ $(document).ready(function(){
         //var topics  = $('.checkbox-topics:checked').serialize();
         var topics = "hello";
         var newtopic = $("#newtopic").val();
-        var newtopictext = $("#newtopictext");
-//        
-//        $.post('ajax_new_scripture.php', {
-//                'book' : book,
-//                'chapter' : chapter,
-//                'verse' : verse,
-//                'content' : content,
-//                'topics'  : topics,
-//                'newtopic' : newtopic,
-//                'newtopictext' : newtopictext
-//            } , function(data){
-//                $("#scripture-list").html(data);
-//            }, 'html'
-//        );
+        var newtopictext = $("#newtopictext").val();
         
-        alert("hej da!");
+        $.post('ajax_new_scripture.php', {
+                'book' : book,
+                'chapter' : chapter,
+                'verse' : verse,
+                'content' : content,
+                'topics'  : topics,
+                'newtopic' : newtopic,
+                'newtopictext' : newtopictext
+            } , function(data){
+                $("#scripture-list").html(data);
+            }, 'html'
+        );
+        
+        alert("wie gehts!");
     });
 });
