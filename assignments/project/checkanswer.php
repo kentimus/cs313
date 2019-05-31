30b5806e3cad6843f9e7a24e2633c9c8e7836856
roperkent <43345600+roperkent@users.noreply.github.com>
2019-05-30 21:41:05 -0700

    working on quiz

--- assignments/project/checkanswer.php
+++ assignments/project/checkanswer.php
@@ -1,1 +1,21 @@
-<pre><?=print_r($_POST);?></pre>
+<?
+session_start();
+$answer = filter_var($_POST['answer'], FILTER_SANITIZE_STRING);
+
+// remove question from beginning of array
+$question = array_shift($_SESSION['vocab_words']);
+
+if($answer == $question['english_word']){
+    // answer was correct
+    $_SESSION['alert'] = "<div class='alert alert-success'>You got it right!</div>";
+} else {
+    // answer was not correct
+    $foreign_word = $question['word'];
+    $english_word = $question['english_word'];
+    $_SESSION['alert'] = "<div class='alert alert-warning'>Nope. The correct translation of <em>$foreign_word</em> is <em>$english_word</em></div>";
+}
+?>
+
+<pre><?=print_r($question);?></pre>
+
+<?=$_SESSION['alert'];?>
