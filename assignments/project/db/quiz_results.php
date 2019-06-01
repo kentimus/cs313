e84f51154aa5f6bc70fc6d9832fbbc26febaaaa0
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-01 08:27:51 -0600

    working on quiz

--- assignments/project/db/quiz_results.php
+++ assignments/project/db/quiz_results.php
@@ -18,13 +18,13 @@
         $query = "INSERT INTO quiz_results
             (vocab_list_id, user_id, score, date)
             VALUES (:vocab_list_id, :user_id, :score, :date)";
-        $statement = $db->prepare($query);
-        $statement->bindValue(':vocab_list_id',$vocab_list_id);
-        $statement->bindValue(':user_id',$user_id);
-        $statement->bindValue(':score',$score);
-        $statement->bindValue(':date',$date);
-        $statement->execute();
-        $statement->closeCursor();
-        return $db->lastInsertId();
+//        $statement = $db->prepare($query);
+//        $statement->bindValue(':vocab_list_id',$vocab_list_id);
+//        $statement->bindValue(':user_id',$user_id);
+//        $statement->bindValue(':score',$score);
+//        $statement->bindValue(':date',$date);
+//        $statement->execute();
+//        $statement->closeCursor();
+//        return $db->lastInsertId();
     }
 ?>
