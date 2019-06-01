c658e7574c59e655cd69516b1f9d610530fc33a6
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-01 09:13:37 -0600

    working on quiz

---
+++ assignments/project/profile_not_logged_in.php
@@ -0,0 +1,5 @@
+<div class="col-md-12">
+    <div class="alert alert-warning">
+        <p><strong>You aren't logged in!</strong> You can't look at your profile page if you aren't logged in. You can fix that by going to the <a href="log_in.php">log in page</a>.</p>
+    </div>
+</div>
