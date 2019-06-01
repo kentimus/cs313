5bce7644c9bd8b445e460b0ab9aa7339ecde9d84
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-01 09:52:22 -0600

    working on quiz

--- assignments/project/ajax_update_email.php
+++ assignments/project/ajax_update_email.php
@@ -1,1 +1,11 @@
-Hi!
+<?
+session_start();
+include("db_connect.php");
+$db = get_db();
+
+echo $_SESSION['user']['id'];
+$user['email'] = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+
+echo "<br>";
+echo $user['email'];
+?>
