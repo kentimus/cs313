b245458aa8f2fe6a509acc436d9dcfb95bc95af1
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-05 07:57:46 -0700

    groupwork week 7

--- groupwork/week7/signin.php
+++ groupwork/week7/signin.php
@@ -15,9 +15,23 @@ include("db_functions.php");
     <body>
         <section class="container">
             <div class="col-md-6 offset-md-3">
-                <h1>Log In!</h1>
+                <h1>Sign In!</h1>
                 
                 <p>(Not signed up yet? <a href="signup.php">Sign up here</a>)</p>
+                
+                <form action="do_login.php" method="post">
+                    <div>
+                        <label for="form-username">Username</label>
+                        <input type="text" class="form-control" id="form-username" name="username">
+                    </div>
+                    <div>
+                        <label for="form-password">Password</label>
+                        <input type="password" class="form-control" id="form-password" name="password">
+                    </div>
+                    <div>
+                        <input type="submit" class="btn btn-primary" value="Sign in">
+                    </div>
+                </form>
             </div>
         </section>
     </body>
