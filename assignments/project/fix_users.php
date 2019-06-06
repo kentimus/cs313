c4eb7fac80fb6c81b949815ffad164fb5a7b9dfa
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-05 18:02:02 -0700

    hashing passwords

---
+++ assignments/project/fix_users.php
@@ -0,0 +1,15 @@
+<? 
+session_start();
+include("db_connect.php");
+$db = get_db();
+
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+        $query = "UPDATE INTO users
+            SET password = :password
+            WHERE id = 1";
+           
+        $statement = $db->prepare($query);
+        $statement->bindValue(':password',$password_hash);
+        $statement->execute();
+        $statement->closeCursor();
+?>
