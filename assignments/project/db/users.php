ebdf5e2e97416d1ee5998fb6e85163db82cd6bdc
roperkent <43345600+roperkent@users.noreply.github.com>
2019-06-05 17:57:25 -0700

    hashing passwords

--- assignments/project/db/users.php
+++ assignments/project/db/users.php
@@ -2,30 +2,35 @@
 function verifyUser($username, $password){
     global $db;
     $query = "SELECT * FROM users
-        WHERE username =  :username
-        AND password = :password";
+        WHERE username =  :username";
     $statement = $db->prepare($query);
     $statement->bindValue(':username',$username);
-    $statement->bindValue(':password',$password);
+    //$statement->bindValue(':password',$password);
     $statement->execute();
     $user = $statement->fetch();
     $statement->closeCursor();
     if(count($user) > 1){
-        return $user;
-    } else {
+        // check to make sure password matched
+        if(password_verify($password, $user['password'])) {
+            return $user;
+        } else { // password doesn't match
+            return false;
+        }
+    } else { // user not found
         return false;
     }
 }
 
 function addUser($data){
     global $db;
+    $password_hash = password_hash($data['password'], PASSWORD_DEFAULT);
     $query = "INSERT INTO users
         (username, email, password)
         VALUES (:username, :email, :password)";
     $statement = $db->prepare($query);
     $statement->bindValue(':username',$data['username']);
     $statement->bindValue(':email',$data['email']);
-    $statement->bindValue(':password',$data['password']);
+    $statement->bindValue(':password',$password_hash);
     $statement->execute();
     $user = $statement->fetch();
     $statement->closeCursor();
